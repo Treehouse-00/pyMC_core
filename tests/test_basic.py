cedb26b4b5c687856bcf6bdd74dc945597d7fc8b
Rightup <lloyd@rightup.co.uk>
2026-09-17 08:44:44 +0100

    chore(version): update version to 1.1.4.dev2

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -2,7 +2,7 @@ from openhop_core import CryptoUtils, LocalIdentity, MeshNode, Packet, __version
 
 
 def test_version():
-    assert __version__ == "1.1.4.dev1"
+    assert __version__ == "1.1.4.dev2"
 
 
 def test_import():
